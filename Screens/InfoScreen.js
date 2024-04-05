f45af44028fb5532a78d93999a7704725d7d9f5a
fathi alaa <202120574khaled@gmail.com>
2024-04-05 14:53:19 +0200

    changes

--- Screens/InfoScreen.js
+++ Screens/InfoScreen.js
@@ -1,50 +1,110 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, Image, Dimensions, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { Ionicons } from '@expo/vector-icons';
+
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
+import Colors from '../Component/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
-export default function InfoScreen({props}) {
-    const navigation=useNavigation()
-    const [rooms,setRooms]=useState([])
-    
-    const getRoomsAndCustomers = async () => {
-      const baseUrl = 'https://7e5f-154-178-181-57.ngrok-free.app';
-    
-      try {
-        // Fetch rooms data
-        const roomsResponse = await fetch(`${baseUrl}/api/rooms`);
-        if (!roomsResponse.ok) {
-          throw new Error('Failed to fetch rooms');
-        }
-        const roomsData = await roomsResponse.json();
-        console.log('Rooms:', roomsData);
-    
-        // Fetch customers data
-        const customersResponse = await fetch(`${baseUrl}/api/customers`);
-        if (!customersResponse.ok) {
-          throw new Error('Failed to fetch customers');
-        }
-        const customersData = await customersResponse.json();
-        console.log('Customers:', customersData);
-      } catch (error) {
-        console.error('Error:', error.message);
-      }
-    };
-    
-    useEffect(() => {
-      // Call the function to fetch data when component mounts
-      getRoomsAndCustomers();
-    }, []);
-    
-    return (
-        <SafeAreaView>
-            <TouchableOpacity
-                onPress={()=>navigation.goBack("")}
-            >
-                <Ionicons name="arrow-back" size={24} color="black" />
-            </TouchableOpacity>
-            <Text>InfoScreen</Text>
-        </SafeAreaView>
-    );
+
+
+export default function InfoScreen() {
+
+const navigation=useNavigation()
+
+  return (
+    <SafeAreaView  style={{backgroundColor:Colors.black,
+      height:hp("100%"),
+      width:wp("100%"),
+      marginTop:"auto",
+      flex:1
+      }} >
+             
+             <TouchableOpacity
+             style={{
+              padding:hp("1.5"),
+              borderRadius:"100%",
+              width:hp("6"),
+              marginLeft:hp("2"),
+             backgroundColor: "#9E44E6",
+             alignItems:"center"
+             
+            }}
+             >
+             <Ionicons name="arrow-back" size={24} color="#ffff" 
+             onPress={()=>navigation.goBack("")}
+             />
+             </TouchableOpacity>
+
+           <ScrollView>
+           <Text style={{color:"#ffff",fontSize:hp('3.5%'),fontWeight:"bold",fontFamily:"koh",marginLeft:"5%",marginBottom:"5%"}}>Lorem ipsum dolor sit amet consectetur.</Text>
+              <TouchableOpacity
+              onPress={()=>navigation.navigate("room")}
+              >
+                <Image  source={require('../assets/images/game.jpg')} style={{
+                  height:hp("35%"),
+                  width:wp("95%"),
+                  marginLeft:hp("1"),
+                  marginRight:hp("2"),
+                  marginBottom:hp("3"),
+                  zIndex:-1
+
+                }} />
+                <Text
+                style={{color:"#ffff",fontSize:hp('3.5%'),fontWeight:"bold",fontFamily:"koh",marginLeft:"5%",marginBottom:"5%",
+              position:"absolute",
+              top:hp("20%")
+              
+              }}
+                >El-Malaab 1</Text>
+                <Text
+                style={{color:"#ffff",fontSize:hp('3.5%'),fontWeight:"bold",fontFamily:"koh",marginLeft:"5%",marginBottom:"5%",
+              position:"absolute",
+              top:hp("25%")
+              
+              }}
+                >10 Available Rooms</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image  source={require('../assets/images/game.jpg')} style={{
+                  height:hp("35%"),
+                  width:wp("95%"),
+                  marginLeft:hp("1"),
+                  marginRight:hp("2"),
+                  
+                  zIndex:-1
+
+                }} />
+                <Text   style={{color:"#ffff",fontSize:hp('3.5%'),fontWeight:"bold",fontFamily:"koh",marginLeft:"5%",marginBottom:"5%",
+              position:"absolute",
+              top:hp("20%")
+              
+              }}>
+                  El-Malaab 2
+                </Text>
+                <Text   style={{color:"#ffff",fontSize:hp('3.5%'),fontWeight:"bold",fontFamily:"koh",marginLeft:"5%",marginBottom:"5%",
+              position:"absolute",
+              top:hp("25%")
+              
+              }}>
+                  Comming Soon..!!!
+                </Text>
+              </TouchableOpacity>
+           </ScrollView>
+            
+          
+            
+           
+          
+        
+             
+              <Image source={require("../assets/images/Ellipse 2.png")} style={{position:"absolute",height:"100%",zIndex:-1, width:"100%" }}   /> 
+        
+        
+          
+        
+      
+    </SafeAreaView>
+  )
 }
